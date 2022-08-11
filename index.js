6442a87f800795487393d64d81b829bba35d74ce
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-11 05:07:15 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code in this file!
-const currentUser = "Edward";
-const welcomeMessage = "Welcome to Flatbook, Edward!";
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, EDWARD!";
-const shortGreeting = "Welcome, E!";
-
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const upperCaseCurrentUser = currentUser.toUpperCase()
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const firstInitial = currentUser.charAt(0)
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
