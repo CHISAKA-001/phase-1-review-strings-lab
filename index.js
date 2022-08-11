53b000b16867b7bc79c64966fa6311d91f59a0f0
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-11 04:36:24 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Edward";
+const welcomeMessage = "Welcome to Flatbook, Edward!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, EDWARD!";
+const shortGreeting = "Welcome, E!";
+
